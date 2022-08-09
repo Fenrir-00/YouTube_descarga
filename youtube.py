3291c47e6e238580bdb75bb8c5a8c2046c49f1ba
Fenrir-00 <96144320+Fenrir-00@users.noreply.github.com>
2022-08-09 21:41:09 +0200

    Add files via upload

---
+++ youtube.py
@@ -0,0 +1,167 @@
+import os, sys, time
+while True:
+ try:
+  from pytube import YouTube
+  break
+ except ModuleNotFoundError:
+  os.system("pip install pytube")
+
+
+class color:
+    morado = '\033[95m'
+    blanco = '\033[97m'
+    cyan = '\033[96m'
+    azul  = '\033[94m'
+    verde = '\033[92m'
+    amarillo = '\033[93m'
+    rojo = '\033[91m'
+    fin = '\033[0m'
+
+
+
+def banner():
+ os.system("clear")
+ print(f"""{color.cyan}
+███████╗███████╗███╗  ██╗██████╗ ██╗██████╗
+██╔════ ██╔════╝████╗ ██║██╔══██╗██║██╔══██╗
+█████╗  █████╗  ██╔██╗██║██████╔╝██║██████╔╝
+██╔══   ██╔══╝  ██║╚████║██╔══██╗██║██╔══██╗
+██║     ███████╗██║ ╚███║██║  ██║██║██║  ██║
+╚═╝     ╚══════╝╚═╝  ╚══╝╚═╝  ╚═╝╚═╝╚═╝  ╚═╝""")
+ print(f"{color.fin}")
+
+def carga():
+    print(f"{color.verde}")
+    print("""Loading…
+    █▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒""")
+    time.sleep(1)
+    os.system("clear")
+    banner()
+    print(f"{color.verde}")
+    print("""Loading…10%
+    ███▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒""")
+    time.sleep(1)
+    os.system("clear")
+    banner()
+    print(f"{color.verde}")
+    print("""Loading…30%
+    █████▒▒▒▒▒▒▒▒▒▒▒▒▒▒""")
+    time.sleep(1)
+    os.system("clear")
+    banner()
+    print(f"{color.verde}")
+    print("""Loading…50%
+    ██████████▒▒▒▒▒▒▒▒▒""")
+    time.sleep(1)
+    os.system("clear")
+    banner()
+    print(f"{color.verde}")
+    print("""Loading…70%
+    █████████████▒▒▒▒▒▒""")
+    time.sleep(1)
+    os.system("clear")
+    banner()
+    print(f"{color.verde}")
+    print("""Loading…100%
+    ███████████████████""")
+    time.sleep(1)
+    os.system("clear")
+    banner()
+def incorrecto():
+    os.system("clear")
+    print(f"""{color.rojo}
+░█████╗░██████╗░░█████╗░██╗░█████╗░███╗░░██╗
+██╔══██╗██╔══██╗██╔══██╗██║██╔══██╗████╗░██║
+██║░░██║██████╔╝██║░░╚═╝██║██║░░██║██╔██╗██║
+██║░░██║██╔═══╝░██║░░██╗██║██║░░██║██║╚████║
+╚█████╔╝██║░░░░░╚█████╔╝██║╚█████╔╝██║░╚███║
+░╚════╝░╚═╝░░░░░░╚════╝░╚═╝░╚════╝░╚═╝░░╚══╝
+██╗███╗░░██╗░█████╗░░█████╗░██████╗░██████╗░
+██║████╗░██║██╔══██╗██╔══██╗██╔══██╗██╔══██╗
+██║██╔██╗██║██║░░╚═╝██║░░██║██████╔╝██████╔╝
+██║██║╚████║██║░░██╗██║░░██║██╔══██╗██╔══██╗
+██║██║░╚███║╚█████╔╝╚█████╔╝██║░░██║██║░░██║
+╚═╝╚═╝░░╚══╝░╚════╝░░╚════╝░╚═╝░░╚═╝╚═╝░░╚═╝
+███████╗░█████╗░████████╗░█████╗░
+██╔════╝██╔══██╗╚══██╔══╝██╔══██╗
+█████╗░░██║░░╚═╝░░░██║░░░███████║
+██╔══╝░░██║░░██╗░░░██║░░░██╔══██║
+███████╗╚█████╔╝░░░██║░░░██║░░██║
+╚══════╝░╚════╝░░░░╚═╝░░░╚═╝░░╚═╝{color.fin}""")
+    time.sleep(4)
+    menu()
+def salir():
+    os.system("clear")
+    banner()
+    print(f"{color.rojo}")
+    print("""SALIENDO…
+    █▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒""")
+    time.sleep(1)
+    os.system("clear")
+    banner()
+    print(f"{color.rojo}")
+    print("""SALIENDO…10%
+    ███▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒""")
+    time.sleep(1)
+    os.system("clear")
+    banner()
+    print(f"{color.rojo}")
+    print("""SALIENDO…30%
+    █████▒▒▒▒▒▒▒▒▒▒▒▒▒▒""")
+    time.sleep(1)
+    os.system("clear")
+    banner()
+    print(f"{color.rojo}")
+    print("""SALIENDO…50%
+    ██████████▒▒▒▒▒▒▒▒▒""")
+    time.sleep(1)
+    os.system("clear")
+    banner()
+    print(f"{color.rojo}")
+    print("""SALIENDO…70%
+    █████████████▒▒▒▒▒▒""")
+    time.sleep(1)
+    os.system("clear")
+    banner()
+    print(f"{color.rojo}")
+    print("""SALIENDO…100%
+    ███████████████████""")
+    time.sleep(1)
+    os.system("clear")
+    print(f"{color.fin}")
+    sys.exit()
+
+def menu():
+    os.system("clear")
+    banner()
+    carga()
+    print(f"{color.morado} DESCARGA  VIDEOS DE YOUTUBE{color.fin}")
+    print("")
+    print(f"{color.cyan}QUE TE GUSTARIA HACER{color.fin}")
+    print(f"{color.verde}[1]DESCARGAR VIDEO")
+    print(f"{color.rojo}[0]SALIR{color.fin}")
+    eleccion =input(f"{color.cyan}ELIJE UN NUMERO >>{color.fin} ")
+    if eleccion == "1" :
+     descargar()
+    elif eleccion == "0" :
+     banner()
+     salir() 
+    else:
+        incorrecto()
+
+def descargar():
+ banner()
+ print()
+ print(f"{color.morado} DESCARGA  VIDEOS DE YOUTUBE{color.fin}")
+ print()
+ url = input("PEGA EL ENLACE DEL VIDEO >> ")
+ print(f"{color.amarillo} TARDARA DEPENDIENDO DE LA DURACION DEL VIDEO{color.fin}")
+ yt = YouTube(url)
+ yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download()
+ os.system("mv *mp4 /data/data/com.termux/files/home/storage/downloads")
+ banner()
+ print(f"{color.verde} VIDEO DESCARGADO EN LA CARPETA DOWNLOADS DE TU TELEFONO{color.fin}")
+ time.sleep(4)
+ menu()
+
+menu()
