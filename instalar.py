bcdf035aa87d17457a04ada5273d2c74158b9c6f
Fenrir-00 <96144320+Fenrir-00@users.noreply.github.com>
2022-12-28 01:00:00 +0100

    Delete instalar.py

--- instalar.py
+++
@@ -1,9 +0,0 @@
-import os , sys
-
-ruta = ":"+os.getenv('PATH')+"/YouTube_descarga"
-os.chdir("..")
-os.system("cp -rf YouTube_descarga /data/data/com.termux/files/usr/bin")
-os.system("rm -rf YouTube_descarga")
-fd = open("/data/data/com.termux/files/usr/etc/bash.bashrc","a")
-fd.write(f"alias youtube='cd /data/data/com.termux/files/usr/bin/YouTube_descarga &&  python youtube.py'")
-fd.close()
